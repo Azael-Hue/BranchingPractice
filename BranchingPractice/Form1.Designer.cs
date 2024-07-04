83fcb5025a9fb7882dfc494cfd0ae00f8c0200f7
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-07-04 10:56:36 -0700

    Added greeting

--- BranchingPractice/Form1.Designer.cs
+++ BranchingPractice/Form1.Designer.cs
@@ -28,18 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnDisplayGreeting = new Button();
+            txtName = new TextBox();
             SuspendLayout();
+            // 
+            // btnDisplayGreeting
+            // 
+            btnDisplayGreeting.Location = new Point(85, 167);
+            btnDisplayGreeting.Name = "btnDisplayGreeting";
+            btnDisplayGreeting.Size = new Size(100, 88);
+            btnDisplayGreeting.TabIndex = 0;
+            btnDisplayGreeting.Text = "Display Greeting";
+            btnDisplayGreeting.UseVisualStyleBackColor = true;
+            btnDisplayGreeting.Click += btnDisplayGreeting_Click;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(85, 63);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(288, 338);
+            Controls.Add(txtName);
+            Controls.Add(btnDisplayGreeting);
             Name = "Form1";
             Text = "Branching Practice";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnDisplayGreeting;
+        private TextBox txtName;
     }
 }
